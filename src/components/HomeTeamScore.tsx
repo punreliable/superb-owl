d48a7563c1f0c69a015715cb35e0f2cd25d8bd4d
Brian <picketfence14@gmail.com>
2024-02-10 17:21:22 -0500

    Testing a basic deployment

---
+++ src/components/HomeTeamScore.tsx
@@ -0,0 +1,14 @@
+import './HomeTeamScore.scss'
+const HomeTeamScore = () => {
+const team = {
+	name: "Kansas City Chiefs",
+}
+
+	return(
+		<div className="homeTeam">
+			<h1>{team.name}</h1>
+		</div>
+
+	)
+}
+export default HomeTeamScore
---
+++ src/components/Scoreboard.tsx
@@ -0,0 +1,10 @@
+import HomeTeamScore from "./HomeTeamScore"
+const Scoreboard = () => {
+return(
+<div className="scoreboard">
+<h1>Scoreboard</h1>
+<HomeTeamScore />
+</div>)
+}
+
+export default Scoreboard
