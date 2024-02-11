af76d7752ede31353f7081b859747ee8784dda9b
Brian <picketfence14@gmail.com>
2024-02-10 20:17:59 -0500

    Fixing Build errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.scss'
 import Scoreboard from './components/Scoreboard'
-import HomeContent from './components/HomeContent'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import './App.scss'
 
 const App = () => {
-
-
-
-  // const [count, setCount] = useState(0)
-
 
   return (
     <>
